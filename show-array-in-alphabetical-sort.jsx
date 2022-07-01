34acfbca46bfc3b392d968db11645221a5f08b2c
Saikat Santra <56018827+SaikatSantra@users.noreply.github.com>
2022-07-01 12:56:24 +0530

    Create show-array-in-alphabetical-sort.jsx

---
+++ show-array-in-alphabetical-sort.jsx
@@ -0,0 +1,18 @@
+const users = [
+  { name: "Akash Doe", id: 1 },
+  { name: "Jane Doe", id: 2 },
+  { name: "Billy Doe", id: 3 }
+];
+const renderList = users.sort((a, b) => a.name.localeCompare(b.name))
+.map((item, index) =><li key={index}>{item.name}-{item.id}</li>);
+
+function App() {
+  return (
+    <>
+      <h3>User names</h3>
+      <ul>
+       {renderList}
+      </ul>
+    </>
+  );
+}
